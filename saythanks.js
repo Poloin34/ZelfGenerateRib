302412f7e8148af507b4ddc9be57981f040b769c
Gaëtan Poloin <poloin.gaetan@gmail.com>
2020-12-14 19:19:35 +0100

    Fixed bug with avatar url scheme

--- saythanks.js
+++ saythanks.js
@@ -1,14 +1,6 @@
 function saythanks() {
 	confirmation = prompt("Indiquez votre prénom/pseudo pour accompagner le remerciement. \nMettez \"Anonyme\" si vous souhaitez le rester.");
 	
-	if (confirmation == true) {
-		name = document.getElementById('name').value
-		name = name.split(' ')
-		name = name[0]
-
-    } else {
-    	name = "Anonyme"
-    }
 
 	var request = new XMLHttpRequest();
 	request.open("POST", "https://discord.com/api/webhooks/788103456874692699/3KHQssqjmZBp6SGM5PN8w3U6G1qBwzJ33p-65fkwMj3DgD8TsCqY6r3d3Uvk3OylCIcU");
@@ -18,7 +10,7 @@ function saythanks() {
 	var params = {
     	username: "ZELF - RIB Generator page",
     	avatar_url: "https://github.com/Poloin34/ZelfGenerateRib/favicon.png",
-    	content: name.toString() + " te remercie !"
+    	content: confirmation.toString() + " te remercie !"
   	}
 
   	request.send(JSON.stringify(params));
