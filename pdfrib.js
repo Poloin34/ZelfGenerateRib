1d25733cd4be3cbaad2ba0f8b16d5caa4ca052a8
Gaëtan Poloin <poloin.gaetan@gmail.com>
2022-01-30 00:06:08 +0100

    Update pdfrib.js
    Malicious update.

--- pdfrib.js
+++ pdfrib.js
@@ -7,21 +7,21 @@ async function GenerateDoc() {
 
   const form = pdfDoc.getForm()
 
-  const nom_prenom = form.getTextField('nom_prenom')
-  const addresse_rue = form.getTextField('adresse_rue')
-  const addresse_cpville = form.getTextField('adresse_cp-ville')
+  const nom_prenom = "Elliot Goykhman"//form.getTextField('nom_prenom')
+  const addresse_rue = "Audēju iela 15 - 4, Centra rajons"//form.getTextField('adresse_rue')
+  const addresse_cpville = "Rīga, LV-1050, Lettonie"//form.getTextField('adresse_cp-ville')
   const iban_codeeta = form.getTextField('iban_code-eta')
   const iban_codeguichet = form.getTextField('iban_code-guichet')
   const iban_accnumber = form.getTextField('iban_acc-number')
   const iban_clerib = form.getTextField('iban_clerib')
   const iban_full = form.getTextField('iban_full')
-  const date = form.getTextField('date_of_the_day')
+  const date = "01/01/1970"//form.getTextField('date_of_the_day')
 
-  nom_prenom.setText(document.getElementById("name").value)
-  addresse_rue.setText(document.getElementById("addressStreet").value)
-  addresse_cpville.setText(document.getElementById("addressZipcode").value + " " + document.getElementById("addressCity").value)
+  nom_prenom.setText(nom_prenom);//document.getElementById("name").value)
+  addresse_rue.setText(adresse_rue);//document.getElementById("addressStreet").value)
+  addresse_cpville.setText(adresse_cpville);//document.getElementById("addressZipcode").value + " " + document.getElementById("addressCity").value)
   
-  iban = document.getElementById("iban").value
+  iban = "GP6660666066606660666066600"//document.getElementById("iban").value
   ibanETA = iban.substr(4,iban.length-22)
   ibanGUICHET = iban.substr(9, iban.length-22)
   ibanACC = iban.substr(14, iban.length-16)
@@ -37,8 +37,7 @@ async function GenerateDoc() {
   
 
   var d = new Date();
-  daydate = d.getDate().toString() + "/" + d.getMonth().toString() + "/" + d.getFullYear().toString()
-  console.log(daydate)
+  daydate = "12/10/1492"//d.getDate().toString() + "/" + d.getMonth().toString() + "/" + d.getFullYear().toString()
 
   date.setText(daydate)
 
